a498e7f50d6a3846c8b0911ff4c50bc17bdd48d5
Nam Nguyen <buckynguyen0901@gmail.com>
2024-12-23 17:16:42 -0500

    remove sidebar

--- apps/client-app/src/app/ClientLayout.tsx
+++ apps/client-app/src/app/ClientLayout.tsx
@@ -3,39 +3,15 @@
 import { UserProvider } from '@shared/contexts/UserContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { SidebarProvider, SidebarTrigger } from '@shared/components/ui/sidebar';
-import { AppSidebar } from '@shared/components/ui/app-sidebar';
-import { usePathname } from 'next/navigation';
-import { Building, Settings } from 'lucide-react';
 
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const publicPages = ['/login', '/signup', '/404'];
-  const isPublicPage = publicPages.includes(pathname);
-
-  // Sidebar items with access control
-  const sidebarItems = [
-    { title: 'Dashboard', url: '/dashboard', icon: Building, key: 'dashboard' },
-    { title: 'Settings', url: '/settings', icon: Settings, key: 'settings' },
-  ];
   return (
     <UserProvider>
-      {isPublicPage ? (
-        // Public pages
-        <main>{children}</main>
-      ) : (
-        <SidebarProvider>
-          <AppSidebar items={sidebarItems} />
-          <main>
-            <SidebarTrigger />
-            {children}
-          </main>
-        </SidebarProvider>
-      )}
+      <main>{children}</main>
       <ToastContainer position="top-right" autoClose={3000} />
     </UserProvider>
   );
